63289aff87e3ecc1afa29fb7823a9004b40c6282
Kolade <kolademodupe007@gmail.com>
2025-02-02 08:13:49 +0100

    finished stag zero

--- lib/main.dart
+++ lib/main.dart
@@ -1,36 +1,62 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('HNG Stage Zero Task'),
+        ),
+        body: const Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              UrlButton(
+                url: 'https://github.com/dartlord007',
+                buttonText: 'GitHub Profile',
+              ),
+              SizedBox(height: 20), // Spacing between buttons
+              UrlButton(
+                url: 'http://hng.tech/hire/flutter-developers',
+                buttonText: 'Hire HNG Flutter Developers',
+              ),
+            ],
+          ),
+        ),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
+/// Centralized widget for handling URL launches
+class UrlButton extends StatelessWidget {
+  final String url;
+  final String buttonText;
 
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
+  const UrlButton({
+    super.key,
+    required this.url,
+    required this.buttonText,
+  });
 
-class _MyHomePageState extends State<MyHomePage> {
+  Future<void> _launchUrl() async {
+    final Uri uri = Uri.parse(url);
+    if (!await launchUrl(uri)) {
+      throw Exception('Could not launch $url');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold( 
+    return ElevatedButton(
+      onPressed: _launchUrl,
+      child: Text(buttonText),
     );
   }
 }
